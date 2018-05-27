dbdaa21e85f4165f5c1e8944d507989be16656a6
IDAlearning <idazou85@gmail.com>
2018-05-28 04:57:48 +0800

    Initial commit

---
+++ testV2/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace testV2
+{
+    partial class Ver2_1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSetColumns = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(630, 253);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 358);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(630, 225);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(659, 99);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(330, 484);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(659, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 65);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(657, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "花費時間";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 37);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(224, 22);
+            this.txtFile.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(223, 20);
+            this.comboBox1.TabIndex = 6;
+            // sthis.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnSetColumns
+            // 
+            this.btnSetColumns.Location = new System.Drawing.Point(242, 35);
+            this.btnSetColumns.Name = "btnSetColumns";
+            this.btnSetColumns.Size = new System.Drawing.Size(75, 23);
+            this.btnSetColumns.TabIndex = 7;
+            this.btnSetColumns.Text = "選取檔案路徑";
+            this.btnSetColumns.UseVisualStyleBackColor = true;
+            this.btnSetColumns.Click += new System.EventHandler(this.btnSetColumns_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "查詢";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(306, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "買賣超前50";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(405, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(581, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "產生";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(405, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "隨機選股";
+            // 
+            // Ver2_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 586);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSetColumns);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Ver2_1";
+            this.Text = "Ver2_1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSetColumns;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ testV2/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testV2
+{
+    public partial class Ver2_1 : Form
+    {
+        // 所有資料的各自list
+        public List<string> DealDate;
+        public List<string> StockID;
+        public List<string> StockName;
+        public List<string> SecBrokerID;
+        public List<string> SecBrokerName;
+        public List<string> Price;
+        public List<string> BuyQty;
+        public List<string> CellQty;
+        //
+
+        public Ver2_1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        public void newLists()
+        {
+            DealDate = new List<string>();
+            StockID = new List<string>();
+            StockName = new List<string>();
+            SecBrokerID = new List<string>();
+            SecBrokerName = new List<string>();
+            Price = new List<string>();
+            BuyQty = new List<string>();
+            CellQty = new List<string>();
+        }
+
+        private void btnSetColumns_Click(object sender, EventArgs e)
+        {
+            // 資料讀取的視窗
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFile.Text = file.SafeFileName;
+                //
+                // 新增所有List
+                newLists();
+                //
+                //清除所有Column, Row
+                dataGridView1.Columns.Clear();
+                dataGridView1.Rows.Clear();
+                //
+                // Add Column
+                AddColumn("DealDate");
+                AddColumn("StocksID");
+                AddColumn("StockName");
+                AddColumn("SecBrokerID");
+                AddColumn("SecBrokerName");
+                AddColumn("Price");
+                AddColumn("BuyQty");
+                AddColumn("CellQty");
+                //
+                // 有檔案就讀入資料
+                if (File.Exists(txtFile.Text))
+                {
+                    // 存入陣列
+                    string[] content = File.ReadAllLines(txtFile.Text, Encoding.GetEncoding("BIG5"));
+                    //
+                    // 開始計算資料讀取速度
+                    Stopwatch stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                    //
+                    // read all data
+                    for (int i = 1; i < content.Length; i++)
+                    {
+                        string[] data = content[i].Split(',');
+                        DealDate.Add(data[0]); // 分別存入DealDate
+                        StockID.Add(data[1]); // 分別存入StockID
+                        StockName.Add(data[2]); // 分別存入StockName
+                        SecBrokerID.Add(data[3]); // 分別存入SecBrokerID
+                        SecBrokerName.Add(data[4]); // 分別存入SecBrokerName
+                        Price.Add(data[5]); // 分別存入Price
+                        BuyQty.Add(data[6]); // 分別存入BuyQty
+                        CellQty.Add(data[7]); // 分別存入CellQty
+
+                        // dataGridView1.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);//將Object Array填入DataRow
+                    }
+                    for (int i = 0; i < 50; i++)
+                    {
+                        dataGridView1.Rows.Add(DealDate[i], StockID[i], StockName[i], SecBrokerID[i], SecBrokerName[i], Price[i], BuyQty[i], CellQty[i]);//將Object Array填入DataRow
+                    }
+                    // dataGridView1.DataSource = content;
+                    // 按照StockID改變順序
+                    dataGridView1.Sort(dataGridView1.Columns[1], ListSortDirection.Ascending);
+                    //
+                    //自動調整寬度
+                    dataGridView1.AutoResizeColumns();
+                    //
+                    // 停止計算讀取速度
+                    stopWatch.Stop();
+                    //
+                    // 顯示讀取速度在textBox1
+                    TimeSpan ts = stopWatch.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+                    textBox1.Text = "讀取檔案: " + elapsedTime;
+                    //
+                }
+                //
+                comboBoxList();
+            }
+
+        }
+        public void AddColumn(string strHeader)
+        {
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.HeaderText = strHeader;
+            dataGridView1.Columns.Add(column);
+        }
+
+        private void comboBoxList()
+        {
+            List<string> lis_DataList = new List<string>();
+            lis_DataList.Add(StockID[1] + "-" + StockName[1]);
+            lis_DataList.Add(StockID[2] + "-" + StockName[2]);
+
+            comboBox1.DataSource = lis_DataList;
+        }
+
+    }
+
+}
